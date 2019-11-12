517b0f923776d6c9c05e5aa22159925af1a1617a
unknown <coderawii@gmail.com>
2019-11-12 21:19:00 +0100

    first part: project- pages, routing, views

---
+++ layouts/error.vue
@@ -0,0 +1,24 @@
+<template>
+<!-- ERROR PAGE tj LAYOUT MORA IMATI IME ERROR, INACE NECE RADITI -->
+  <div class="error-page">
+    <h1>Oops, something went wrong!</h1>
+    <p>Back to <a href="/">safety</a>!</p>
+	 <!-- ovde stavljamo anchor tag sa linkom za vracanje na nasu app sa normalnim href atributom jer moguce da smo dosli do error page jer je nas js crashovao onda nuxt-link takodje ne bi radio jer je nasa js aplikacija mozda mrtva, pa da bi omogucili vracanje na nas page ipak idemo sa href jer tako reloadujemo nasu app -->
+  </div>
+</template>
+
+<style scoped>
+.error-page {
+  text-align: center;
+}
+
+.error-page a {
+  text-decoration: none;
+  color: red;
+}
+
+.error-page a:hover,
+.error-page a:active {
+  color: salmon;
+}
+</style>
---
+++ pages/about/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="about-page">
+		<h1>This is my absolutely fantastic "About Me" Page!</h1>
+		<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas sint molestias beatae, at nihil ipsum ipsa. A quis sint aspernatur repudiandae consequatur. Minima ab ex libero id nam repudiandae quis.
+		</p>
+
+		<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas sint molestias beatae, at nihil ipsum ipsa. A quis sint aspernatur repudiandae consequatur. Minima ab ex libero id nam repudiandae quis.
+		</p>
+
+		<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas sint molestias beatae, at nihil ipsum ipsa. A quis sint aspernatur repudiandae consequatur. Minima ab ex libero id nam repudiandae quis.
+		</p>
+
+		<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas sint molestias beatae, at nihil ipsum ipsa. A quis sint aspernatur repudiandae consequatur. Minima ab ex libero id nam repudiandae quis.
+		</p>
+		
+	</div>
+</template>
+
+<style scoped>
+.about-page {
+	text-align: center;
+	padding: 30px;
+}
+</style>
